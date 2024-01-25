867a0b9c1b0934ff2b6877d6222f7bfb5892b54e
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-01-25 13:28:28 +0700

    Remove api

--- src/main/java/com/shopee/ecommer/shopeebeaccountdemo/constant/PathApi.java
+++ src/main/java/com/shopee/ecommer/shopeebeaccountdemo/constant/PathApi.java
@@ -2,10 +2,4 @@ package com.shopee.ecommer.shopeebeaccountdemo.constant;
 
 public interface PathApi {
     String LOGIN_PATH = "/login";
-
-    String ACCOUNT_PATH = "/account";
-
-    String CREATE = "/create";
-
-    String LIST = "/list";
 }
