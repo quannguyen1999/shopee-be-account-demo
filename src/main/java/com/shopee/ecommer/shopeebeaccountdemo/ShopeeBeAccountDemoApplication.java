6e1bdffa078d99c6b519b2317b1e3fe75c54bb59
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-03-05 09:25:58 +0700

    add eureka

--- src/main/java/com/shopee/ecommer/shopeebeaccountdemo/ShopeeBeAccountDemoApplication.java
+++ src/main/java/com/shopee/ecommer/shopeebeaccountdemo/ShopeeBeAccountDemoApplication.java
@@ -2,8 +2,10 @@ package com.shopee.ecommer.shopeebeaccountdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ShopeeBeAccountDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShopeeBeAccountDemoApplication.class, args);
