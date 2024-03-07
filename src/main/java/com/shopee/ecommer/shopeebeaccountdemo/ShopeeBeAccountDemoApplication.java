6cb3216201d4b035dd2d5d94d156f12b026b9079
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-03-07 10:26:10 +0700

    add rate limit

--- src/main/java/com/shopee/ecommer/shopeebeaccountdemo/ShopeeBeAccountDemoApplication.java
+++ src/main/java/com/shopee/ecommer/shopeebeaccountdemo/ShopeeBeAccountDemoApplication.java
@@ -3,9 +3,9 @@ package com.shopee.ecommer.shopeebeaccountdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ShopeeBeAccountDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShopeeBeAccountDemoApplication.class, args);
