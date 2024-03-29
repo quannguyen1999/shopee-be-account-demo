273fce9b9aba621e3142c62934248b33726b7e87
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-03-29 08:41:54 +0700

    Fix Bug

--- src/main/java/com/shopee/ecommer/shopeebeaccountdemo/service/UserDetailConfigService.java
+++ src/main/java/com/shopee/ecommer/shopeebeaccountdemo/service/UserDetailConfigService.java
@@ -4,19 +4,12 @@ import com.shopee.ecommer.shopeebeaccountdemo.entity.Account;
 import com.shopee.ecommer.shopeebeaccountdemo.entity.CustomUserDetails;
 import com.shopee.ecommer.shopeebeaccountdemo.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.encrypt.BytesEncryptor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
-
-import java.util.Collection;
-import java.util.HashSet;
 
 @RequiredArgsConstructor
 @Service
@@ -27,13 +20,10 @@ public class UserDetailConfigService implements UserDetailsService {
     @Transactional
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
         Account account = accountRepository.findByUsername(username);
         if (ObjectUtils.isEmpty(account)) {
             throw new UsernameNotFoundException("Access Denied " + username);
         }
-        Collection<GrantedAuthority> authorities = new HashSet<>();
-        account.getRoleAccountList().forEach(auth -> authorities.add(new SimpleGrantedAuthority(auth.getCode())));
         return new CustomUserDetails(
                 new Account(account.getId(), account.getUsername(), account.getPassword(), account.getBirthday(),
                         account.getGender(), account.getEmail(), account.getAvatar(), account.getIsActive(),
